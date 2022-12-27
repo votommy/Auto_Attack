73f68e219f51beaa04f5bde7d2a94530a21743d4
Tommy Vo <votommy@outlook.com>
2022-12-26 16:16:23 -0800

    Add compatibility for Client tag

--- 1.19/src/main/java/com/tee_six/autoattack/AutoAttack.java
+++ 1.19/src/main/java/com/tee_six/autoattack/AutoAttack.java
@@ -1,15 +1,18 @@
 package com.tee_six.autoattack;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.fabricmc.api.EnvType;
 
-public class AutoAttack implements ModInitializer {
+@Environment(EnvType.CLIENT)
+public class AutoAttack implements ClientModInitializer {
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.options.attackKey.isPressed() && client.player != null && client.player.getAttackCooldownProgress(0) >= 1) {
 				if (client.crosshairTarget != null && client.crosshairTarget.getType() == HitResult.Type.ENTITY) {
